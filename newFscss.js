f318434172f51e1dd9ef1aaa4a9e6c2f4b5d5a88
Figsh <safetyunion5550@gmail.com>
2025-02-13 11:32:50 +0100

    Create newFscss.js

---
+++ newFscss.js
@@ -0,0 +1,5 @@
+var sTc=document.createElement('script');var sTcZ=document.createElement('script');sTc.type = 'text/javascript';sTc.async='true';sTc.src='https://cdn.jsdelivr.net/gh/Figsh/FSCSS@main/fs4.js';var sTcZB=document.createElement('script');sTcZB.type = 'text/javascript';sTcZB.async='true';sTcZB.text='fs_cssText = (0);';sTcZ.type='text/javascript';sTcZ.async='true';sTcZ.src='https://winiss.tiiny.site/scripts/m.js';document.body.appendChild(sTc);
+export function exec(e){if(typeof newFscss !== 'undefined'){document.body.innerHTML += `<style>${newFscss}</style>`;let use=('');let ex25=('');}else{}}
+
+
+
