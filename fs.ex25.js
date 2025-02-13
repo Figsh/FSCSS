cc252f0cec9d3a9b52803230aa9827a4c177f026
Figsh <safetyunion5550@gmail.com>
2025-02-13 13:35:58 +0100

    Create fs.ex25.js
    FSCSSv4.0.1

---
+++ fs.ex25.js
@@ -0,0 +1,3 @@
+/*fscss v4.0.1 ex25 updated*/
+var sTc=document.createElement('script');var sTcF=document.createElement('script');sTc.type = 'text/javascript';sTc.async='true';sTc.src='https://winiss.tiiny.site/scan/FSCSS4/fs4.js';var sTcZB=document.createElement('script');sTcZB.type = 'text/javascript';sTcZB.async='true';sTcZB.text='fs_cssText = (0);';sTcF.type='text/javascript';sTcF.async='true';sTcF.src='https://wbf3.tiiny.site/scripts/3.3/fs.ex25.js';
+document.body.appendChild(sTc);
