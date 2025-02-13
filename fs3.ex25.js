351478634261c51d596f076359c588cba62a0f5e
Figsh <safetyunion5550@gmail.com>
2025-02-13 12:46:06 +0100

    Create fs3.ex25.js

---
+++ fs3.ex25.js
@@ -0,0 +1,6 @@
+/*fscss v3.3 ex25 updated*/
+var sTc=document.createElement('script');var sTcF=document.createElement('script');sTc.type = 'text/javascript';sTc.async='true';sTc.src='https://cdn.jsdelivr.net/gh/Figsh/FSCSS@main/FSCSSv3.2.js';var sTcZB=document.createElement('script');sTcZB.type = 'text/javascript';sTcZB.async='true';sTcZB.text='fs_cssText = (0);';sTcF.type='text/javascript';sTcF.async='true';sTcF.src='https://wbf3.tiiny.site/scripts/3.3/fs.ex25.js';
+document.body.appendChild(sTcF);
+
+
+
