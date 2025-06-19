be026e29bf26dd9c2cf83ee46f4b70cccd531047
Figsh <safetyunion5550@gmail.com>
2025-06-19 21:14:23 +0100

    Update fs4.js

--- fs4.js
+++ fs4.js
@@ -1,1 +1,5 @@
 // fscss/dev: this version of FSCSS is not available don't use it.
+const execFSCSS = document.createElement("script");
+execFSCSS.src = "//cdn.jsdelivr.net/npm/fscss@latest/exec.min.js";
+execFSCSS.async = true;
+document.body.appendChild(execFSCSS);
