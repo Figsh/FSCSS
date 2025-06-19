3cd70d765bfde95ecae1f2f5c38c3d052ba6d47b
Figsh <safetyunion5550@gmail.com>
2025-06-19 20:12:00 +0100

    Update fs3.ex25.js

--- fs3.ex25.js
+++ fs3.ex25.js
@@ -1,5 +1,5 @@
-/*fscss v3.3 ex25 updated*/
-var sTc=document.createElement('script');var sTcF=document.createElement('script');sTc.type = 'text/javascript';sTc.async='true';sTc.src='https://winiss.tiiny.site/scripts/fscssv3.1m.js';var sTcZB=document.createElement('script');sTcZB.type = 'text/javascript';sTcZB.async='true';sTcZB.text='fs_cssText = (0);';sTcF.type='text/javascript';sTcF.async='true';sTcF.src='https://wbf3.tiiny.site/scripts/3.3/fs.ex25.js';
+/*fscss latest ex25 updated*/
+var sTc=document.createElement('script');var sTcF=document.createElement('script');sTc.type = 'text/javascript';sTc.async='true';sTc.src='https://winiss.tiiny.site/scripts/fscssv3.1m.js';var sTcZB=document.createElement('script');sTcZB.type = 'text/javascript';sTcZB.async='true';sTcZB.text='fs_cssText = (0);';sTcF.type='text/javascript';sTcF.async='true';sTcF.src='//cdn.jsdelivr.net/npm/fscss@latest/exec.min.js';
 document.body.appendChild(sTcF);
 
 
