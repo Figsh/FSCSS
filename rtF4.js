bf30dab56141f551897b7394dd54c76cb530dde2
Figsh <safetyunion5550@gmail.com>
2025-06-22 23:02:54 +0100

    Update rtF4.js

--- rtF4.js
+++ rtF4.js
@@ -17,7 +17,7 @@
  * Note: Use official npm package/CDN instead of copying this directly.
  *       Contact: Facebook (exFSCSS) for support.
  */function transformCssValues(css){let customProperties=new Set();let transformedCss=css;const copyRegex=/(:\s*)(["']?)(.*?)(["']?)\s*copy\(([-]?\d+),\s*([^\;^\)^\(^,^ ]*)\)/g;transformedCss=transformedCss.replace(copyRegex,(match,prefix,quote1,value,quote2,lengthStr,variableName)=>{const length=parseInt(lengthStr);let extractedValue='';if(length>=0){extractedValue=value.substring(0,length);}else{extractedValue=value.substring(value.length+length);}customProperties.add(`--${variableName.replace(/[^a-zA-Z0-9_-]/g,'')}:${extractedValue};`);return `${prefix}${quote1}${value}${quote2}`;});if(customProperties.size>0){const rootBlock=`:root{\n${Array.from(customProperties).join('\n')}\n}`;transformedCss+=`\n${rootBlock}`;}return transformedCss;} function repeatString(str,count){str=str.replace(/^['"]|['"]$/g,'');return str.repeat(Math.max(0,parseInt(count)))}function replaceRe(css) {
-  const reRegex = /re\(\s*([^:),]+)\s*[,:]\s*(?:"([^"]*)"|'([^']*)')\s*\)/gi;
+  const reRegex = /(?:store|str|re)\(\s*([^:,]+)\s*[,:]\s*(?:"([^"]*)"|'([^']*)')\s*\)/gi;
   const variableMap = new Map();
   let cleanedCss = css.replace(reRegex, (match, variable, dqValue, sqValue) => {
     const value = dqValue || sqValue;
