52e5d8ec9f5304941d5f384f056511e20dbb2da1
Figsh <safetyunion5550@gmail.com>
2025-06-29 23:19:49 +0100

    Update and rename index.js to index.js

--- .figsh/index.js
+++ .figsh/index.js
@@ -1,4 +1,4 @@
 const execFSCSS = document.createElement("script");
-execFSCSS.src = "//cdn.jsdelivr.net/npm/fscss@latest/exec.min.js";
+execFSCSS.src = "//cdn.jsdelivr.net/npm/fscss@1.1.6/e/exec.min.js";
 execFSCSS.async = true;
 document.body.appendChild(execFSCSS);
