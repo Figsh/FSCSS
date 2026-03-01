6f29927343749b38d32ce49e3801f0d5cc574923
Figsh <safetyunion5550@gmail.com>
2026-03-02 00:26:24 +0100

    Delete fs3.ex25.js

--- fs3.ex25.js
+++
@@ -1,6 +0,0 @@
-/*fscss latest ex25 updated*/
-var sTc=document.createElement('script');var sTcF=document.createElement('script');sTc.type = 'text/javascript';sTc.async='true';sTc.src='https://winiss.tiiny.site/scripts/fscssv3.1m.js';var sTcZB=document.createElement('script');sTcZB.type = 'text/javascript';sTcZB.async='true';sTcZB.text='fs_cssText = (0);';sTcF.type='text/javascript';sTcF.async='true';sTcF.src='//cdn.jsdelivr.net/npm/fscss@latest/exec.min.js';
-document.body.appendChild(sTcF);
-
-
-
